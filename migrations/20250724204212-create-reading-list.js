ddf095814445fd9f331aa35ef18f4b0c85fa2fa5
JanakaAlwis <janaka.donmanuwelge@student.hamk.fi>
2025-07-24 23:56:28 +0300

    migration of the reading list table

---
+++ migrations/20250724204212-create-reading-list.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('ReadingLists', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      userId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+      },
+      blogId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'Blogs',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+      },
+      read: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('ReadingLists');
+  },
+};
